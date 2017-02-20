dfeec09999c42a524c5523e9cd0f910cf61b6a37
PoojanParikh <camkampar@gmail.com>
2017-02-20 14:03:35 +0530

    Custom Adapters Created

---
+++ app/src/main/java/com/vnurture/vnurture/quote2/ListCustomAdapter.java
@@ -0,0 +1,31 @@
+package com.vnurture.vnurture.quote2;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by VNurtureTechnologies on 20/02/17.
+ */
+
+public class ListCustomAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}

6a99ec494ac7106a77412771c0d98f1b1178a5d7
PoojanParikh <camkampar@gmail.com>
2017-02-20 14:02:09 +0530

    Full Page Quote fragment is Created

---
+++ app/src/main/java/com/vnurture/vnurture/quote2/FullPageQuoteFragment.java
@@ -0,0 +1,10 @@
+package com.vnurture.vnurture.quote2;
+
+import android.app.Fragment;
+
+/**
+ * Created by VNurtureTechnologies on 20/02/17.
+ */
+
+public class FullPageQuoteFragment extends Fragment {
+}
